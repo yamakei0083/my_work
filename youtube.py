8b578bad7fe92894efda9a9e435e0eb1b124d48b
yamakei0083 <39458262+yamakei0083@users.noreply.github.com>
2018-06-04 13:32:50 +0900

    Add files via upload

---
+++ youtube.py
@@ -0,0 +1,11 @@
+import time
+import webbrowser
+
+total_breaks = 3
+break_count = 0
+
+print("This program started on " + time.ctime())
+while(break_count < total_breaks):
+    time.sleep(2)
+    webbrowser.open("https://www.youtube.com/watch?v=uafJpeeCwTw&start_radio=1&list=RDuafJpeeCwTw")
+    break_count += 1
